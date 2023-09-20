a8dd6f91729e3dd2fad1050344fb8b3df115a1fd
Wizard Today <aleshko.vitali@gmail.com>
2023-09-20 22:12:17 +0300

    4 (test)

--- start.ts
+++ start.ts
@@ -3,9 +3,11 @@ import { settings } from './src/settings.ts'
 import { useButtonActions } from './src/actions/button.ts'
 import { useStartAction } from './src/actions/start.ts'
 
+console.log(settings)
+
 const bot = new Telegraf(settings.token)
 
 useStartAction(bot)
 useButtonActions(bot)
 
-bot.launch()
+bot.launch().then(() => console.log('Bot started!'))
