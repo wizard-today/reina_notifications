1aa23408c01093f1f4f55d85e0e9ee9c1f2eded8
Wizard Today <aleshko.vitali@gmail.com>
2023-09-20 22:16:08 +0300

    5 (test)

--- start.ts
+++ start.ts
@@ -1,13 +1,15 @@
-import { Telegraf } from 'npm:telegraf'
-import { settings } from './src/settings.ts'
-import { useButtonActions } from './src/actions/button.ts'
-import { useStartAction } from './src/actions/start.ts'
+// import { Telegraf } from 'npm:telegraf'
+// import { settings } from './src/settings.ts'
+// import { useButtonActions } from './src/actions/button.ts'
+// import { useStartAction } from './src/actions/start.ts'
 
-console.log(settings)
+// console.log(settings)
 
-const bot = new Telegraf(settings.token)
+// const bot = new Telegraf(settings.token)
 
-useStartAction(bot)
-useButtonActions(bot)
+// useStartAction(bot)
+// useButtonActions(bot)
 
-bot.launch().then(() => console.log('Bot started!'))
+// bot.launch().then(() => console.log('Bot started!'))
+
+console.log('test')
